09d63db7f572e7388ce3bab34322c86f42b01476
james713 <contact@jamessliu.com>
2017-09-13 23:21:07 +0000

    Update test for parser

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -24,49 +24,49 @@ defmodule ParserTest do
   test "parse_channel works on names" do
     channel = %Channel{ id: 274989267086868480, name: "bot-stuff" }
     guild = %Guild{ channels: [channel] }
-    assert CommandParser.parse_channel("bot-stuff", guild) == {:ok, channel}
+    assert CommandParser.parse_channel("bot-stuff", guild) == channel
   end
 
   test "parse_member works on usernames" do
     member = %Member{ user: %User{ username: "GeorgePBurdell", id: 274989267086868480 }}
     guild = %Guild{ members: [member] }
-    assert CommandParser.parse_guild_member("GeorgePBurdell", guild) == {:ok, member}
+    assert CommandParser.parse_guild_member("GeorgePBurdell", guild) == member
   end
 
   test "parse_member works on nicknames" do
     member = %Member{ nick: "Reimu Hakurei", user: %User{ id: 274989267086868480 }}
     guild = %Guild{ members: [member] }
-    assert CommandParser.parse_guild_member("Reimu Hakurei", guild) == {:ok, member}
+    assert CommandParser.parse_guild_member("Reimu Hakurei", guild) == member
   end
 
   test "parse_role works on role names" do
     role = %Role{ id: 274989267086868480, name: "Moderator" }
     guild = %Guild{ roles: [role] }
-    assert CommandParser.parse_role("Moderator", guild) == {:ok, role}
+    assert CommandParser.parse_role("Moderator", guild) == role
   end
 
   test "parse_channel works on mentions" do
     channel = %Channel{ id: 274989267086868480 }
     guild = %Guild{ channels: [channel] }
-    assert CommandParser.parse_channel("<#274989267086868480>", guild) == {:ok, channel}
+    assert CommandParser.parse_channel("<#274989267086868480>", guild) == channel
   end
 
   test "parse_member works on mentions" do
     member = %Member{ user: %User{ id: 274989267086868480 }}
     guild = %Guild{ members: [member] }
-    assert CommandParser.parse_guild_member("<@274989267086868480>", guild) == {:ok, member}
+    assert CommandParser.parse_guild_member("<@274989267086868480>", guild) == member
   end
 
   test "parse_member works on nickname mentions" do
     member = %Member{ user: %User{ id: 274989267086868480 }}
     guild = %Guild{ members: [member] }
-    assert CommandParser.parse_guild_member("<@!274989267086868480>", guild) == {:ok, member}
+    assert CommandParser.parse_guild_member("<@!274989267086868480>", guild) == member
   end
 
   test "parse_role works on mentions" do
     role = %Role{ id: 274989267086868480 }
     guild = %Guild{ roles: [role] }
-    assert CommandParser.parse_role("<@&274989267086868480>", guild) == {:ok, role}
+    assert CommandParser.parse_role("<@&274989267086868480>", guild) == role
   end
 
   test "parse_channel works on ids" do
